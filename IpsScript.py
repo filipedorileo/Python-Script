7430aad7841f2b15fec0a9723637cda41c42f882
filipedorileo <108145523+filipedorileo@users.noreply.github.com>
2022-06-28 12:29:07 +0100

    Update

--- IpsScript.py
+++ IpsScript.py
@@ -1,81 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import pandas as pd
 
-
-
-
-
-dataFrom_IPs  = pd.read_csv(r'C:\Users\xxx\Desktop\Ips.csv',sep=';')
+dataFrom_IPs = pd.read_csv(r'C:\Users\Findmore\Desktop\Ips.csv', sep=';')
 listA = []
-listB=[]
-Equals=[]
-Diff=[]
-
-
-
+listB = []
+Equals = []
+Diff = []
 
 
 dataFrom_IPs
 
 
-
-
-list_Host  = dataFrom_IPs['host'].tolist()
+list_Host = dataFrom_IPs['Host'].tolist()
 
 list_Host
 
+list_Ips = dataFrom_IPs['Ips']
+
+
+listEquals = []
+listDiff = []
+temp_list = []
+listB = []
+
+
+list_Host  # Lista de todos os objetos da Coluna 'host'
+list_Ips  # Lista de todos os objetos da Coluna 'Ips'
+
+# listB=list_Ips[0]
+#listB = listB.split(",")
 
 
 
 
-list_Ips  = dataFrom_IPs['Ips']                  
+for i, iteminHost in enumerate(list_Host):
+    print('-----------------------------------------------------')
+    print('Host:'+iteminHost)
+    for index, row in enumerate(list_Ips):
+
+        listB = list_Ips[index]
+        print('')
+        print(listB)
+        if iteminHost in listB:
+
+            dataFrom_IPs.at[index, 'Equals'] = iteminHost
+
+        else:
+            tempA = listB
+            tempA = tempA.replace(iteminHost, '')
+            print('#######')
+            print('tempA:  '+tempA)
+            print('#######')
+
+            dataFrom_IPs.at[index, 'Diff'] = tempA
 
 
-
-
-listEquals=[]
-listDiff=[]
-temp_list=[]
-listB=[]
-
-
-
-
-
-list_Host #Lista de todos os objetos da Coluna 'host'
-list_Ips #Lista de todos os objetos da Coluna 'Ips'
-
-
-for i,iteminHost in enumerate(list_Host):
-        print('-----------------------------------------------------')
-        print('Host:'+iteminHost)
-        for index, row in enumerate(list_Ips):
-            
-            listB=list_Ips[index]
-            print('')   
-            print(listB)
-            if iteminHost in listB:
-                
-                tempA = listB
-                
-                
-                
-                dataFrom_IPs.at[index, 'Equals']= iteminHost
-                
-          
-                  
-            else:
-                tempA.replace(iteminHost,'')
-                dataFrom_IPs.at[index, 'Diff']=tempA    
-
-
-
-#listEquals  
 dataFrom_IPs.head(18)
 
-
-
+dataFrom_IPs.at[2, 'Diff']
